d7ffb64f3ca631ff6549c1b80e81d3d6555b28a2
leouhre <leouhrejakobsen@gmail.com>
2022-04-01 14:53:22 +0200

    new data to matlab

--- matlab/tf_estimate.m
+++ matlab/tf_estimate.m
@@ -1,9 +1,15 @@
-fileID = fopen('step_response/sensor6.txt', 'r');
+fileID = fopen('step_response/sensor7.txt', 'r');
 
 formatSpec = '%d %f';
 
 sizeA = [2 Inf];
 
-A = fscanf(fileID,formatSpec,sizeA)
+A = fscanf(fileID,formatSpec,sizeA);
 
 fclose(fileID);
+
+x = 17*ones(1,length(A));
+
+data = iddata(A(2,:),x)
+
+tfestimate(x,A(2,:))
