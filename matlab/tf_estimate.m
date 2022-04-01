a4a8e2e966a0f437d787c320a9df6653c537ad46
leouhre <leouhrejakobsen@gmail.com>
2022-04-01 16:01:49 +0200

    nice transfer function

--- matlab/tf_estimate.m
+++ matlab/tf_estimate.m
@@ -1,21 +1,26 @@
-load('step_response/sensor5.txt');
+clear all
 
-%fileID = fopen('step_response/sensor7.txt', 'r');
+% Get step response from data file and save in variable 'temp'
+fileID = fopen('step_response/sensor5.txt', 'r');
+formatSpec = '%d %f';
+sizeA = [2 Inf];
+temp = fscanf(fileID,formatSpec,sizeA);
+fclose(fileID);
 
-%formatSpec = '%d %f';
+% Create input/output vectors for the tfest function
+% Remember to subtract room temperature temp(2,1)
+u = transpose([zeros(1,length(temp)),17*ones(1,length(temp))]);
+y = transpose([zeros(1,length(temp)),temp(2,:) - temp(2,1)]);
 
-%sizeA = [2 Inf];
-
-%A = fscanf(fileID,formatSpec,sizeA);
-
-%fclose(fileID);
-
-x = zeros(length(sensor5),1);
-
-x = (zeros(length(sensor5),1),17*ones(length(sensor5),1));
-
-A = [ones(length(sensor5),1)*sensor5(1,2),sensor5(:,2)];
-
-data = iddata(sensor5(:,2),x,1);
-
-tfestimate(x,A(2,:))
+% Estimate the transfer function for the system (np = number of poles)
+data = iddata(y,u,1);
+np = 6;
+sys = tfest(data,np);
+figure(1)
+bode(sys)
+figure(2)
+opt = stepDataOptions('StepAmplitude',17);
+step(sys,opt)
+%hold on
+%plot(temp(2,:) - temp(2,1))
+%hold off
