2096146613c40df7f65f45dce7f0a807188a6e80
theodorgto <66820813+theodorgto@users.noreply.github.com>
2022-04-04 16:18:36 +0200

    PID added to testscript #added by theo

--- pid_Test.py
+++ pid_Test.py
@@ -1,70 +1,49 @@
-
-
-import time
-
+import time, sys
+import matplotlib.pyplot as plt,numpy as np
 
 class PID():
-    def __init__(self, kp, taui):
-        self.kp = kp
-        self.taui = taui
-        self.ki = kp/taui
+    def __init__(self):
+        config = open('config.txt', 'r')
+        self.kp = float(config.readline())
+        self.taui = float(config.readline())
+        #self.kp = kp
+        #self.taui = taui
+        self.ki = self.kp/self.taui
 
     integral_error = 0
     error = 0
     
     def update_error(self,t,t_target):
         self.error = t_target - t
-        if abs(self.error) < 5: # only activate integrator if error is less than 5 C
+        if abs(self.error) < 10: # only activate integrator if error is less than 5 C
             self.integral_error = self.integral_error + self.error
         else:
             self.integral_error = 0 # reset      #TODO check if this causes errors later?
+    
+    def integral(self):
+        return self.ki*self.integral_error
 
 
-
-def integral(PI:PID):
-    return PI.ki*PI.integral_error
-
-
-def proportional(PI:PID):
-    return PI.kp*PI.error
+    def proportional(self):
+        return self.kp*self.error
 
 
 timerA = time.time()
-t_target = 90
+t_target = float(sys.argv[1])
 t = 0
-PI = PID(0.2,2)
 
-
-def get_pid_parameters():
-    config = open('config.txt', 'r')
-    kp = config.readline()
-    taui = config.readline()
-
-    print(str(kp))
-    print(str(taui))
-    print(str(kp+taui))
-
-
-get_pid_parameters()
+PI = PID()
     
 
 while True:
     if (time.time() - timerA) > 2:
         timerA = time.time()
-        # TODO PID
-        PI.update_error(t,t_target)
-        if (abs(t_target - t) < 1):
-            break
-        t = t + max(min(proportional(PI) + integral(PI),28),0) #saturation at 28V and o lower than 0V
 
-        print("target " + str(t_target))
-        print("actual " + str(t))
+        PI.update_error(t,t_target)
+        t = t + max(min(PI.proportional() + PI.integral(),28),0) #saturation at 28V and o lower than 0V
+
+        print("tempearture " + str(t))
 
         t = t - 2
-
-
-print("target " + str(t_target))
-print("actual " + str(t))
-    
   
 
