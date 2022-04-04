742dfa66b99afa09e6aca6f2da0b9398c8f3c836
leouhre <leouhrejakobsen@gmail.com>
2022-04-04 16:52:47 +0200

    Update main.m

--- matlab/main.m
+++ matlab/main.m
@@ -9,3 +9,5 @@ T = string(input(prompt));
 % run Python script on Raspberry Pi creating data.txt
 %system(r,'python3 ~/Desktop/Rb_Controller/pid_Test.py ' + T)
 system('python3 pid_Test.py ' + T)
+
+input();
