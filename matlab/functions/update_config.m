52daf58dcfc33e12d0a2f33ac3a51bf59fdd405b
leouhre <leouhrejakobsen@gmail.com>
2022-04-04 16:50:37 +0200

    calibrate begun

---
+++ matlab/functions/update_config.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = update_config(inputArg1mypi,inputArg2)
+%UPDATE_CONFIG Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
