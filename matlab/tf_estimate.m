1da562daac4cdea71f62225994be32dda7e21706
leouhre <leouhrejakobsen@gmail.com>
2022-04-04 12:17:59 +0200

    working PI controller

--- matlab/tf_estimate.m
+++ matlab/tf_estimate.m
@@ -1,6 +1,8 @@
 %% Simulink model name
 model='system_model.m';
 
+s=tf('s');
+
 % Get step response from data file and save in variable 'temp'
 fileID = fopen('step_response/sensor5.txt', 'r');
 formatSpec = '%d %f';
@@ -17,11 +19,11 @@ y = transpose([zeros(1,length(temp)),temp(2,:) - temp(2,1)]);
 data = iddata(y,u,1);
 np = 4;
 sys = tfest(data,np);
-figure(1)
-bode(sys)
-figure(2)
+%figure(1)
+%bode(sys)
+%figure(2)
 opt = stepDataOptions('StepAmplitude',175);
-step(sys,opt)
+%step(sys,opt)
 %hold on
 %plot(temp(2,:) - temp(2,1))
 %hold off
@@ -29,28 +31,30 @@ step(sys,opt)
 num = sys.Numerator;
 den = sys.Denominator;
 G = tf(num,den);
-kp = 1;
 
-Ni_vel = 6; % prioritize fast response rather
-alpha_vel = 0.05; % than small overshoot
-pm_vel = 65;
+Ni = 6; % prioritize fast response rather
+alpha = 0.05; % than small overshoot
+pm = 65;
 
-rhoi_vel = rad2deg(-atan(1/Ni_vel));
-rhom_vel = rad2deg(asin((1-alpha_vel)/(1+alpha_vel)));
-rhoG_vel = pm_vel - rhoi_vel - 180 - rhom_vel;
+rhoi = rad2deg(-atan(1/Ni));
+rhom = rad2deg(asin((1-alpha)/(1+alpha)));
+rhoG = pm - rhoi - 180 - rhom;
 
 % find new crossover freq from bode plot
-bode(Gbal*Kp*CI)
-%wc_vel = 3.7;
+% bode(G)
+wc = getGainCrossover(G,1);
 
-%td_vel = 1/(wc_vel*sqrt(alpha_vel));
-%ti_vel = Ni_vel*1/wc_vel;
-%CD_vel = (td_vel*s + 1)/(alpha_vel*td_vel*s + 1);
-%CI_vel = (ti_vel*s + 1)/(ti_vel*s);
+td = 1/(wc*sqrt(alpha));
+ti = Ni*1/wc;
+CD = (td*s + 1)/(alpha*td*s + 1);
+CI = (ti*s + 1)/(ti*s);
+CI_num = cell2mat(CI.Numerator);
+CI_den = cell2mat(CI.Denominator);
 
 % find Kp2 from bode plot
 %bode(Gbal*Kp*CI*CD_bal*CI2_bal);
-%Kp_vel = 1/db2mag(12);
+ %kp = db2mag(30);
+kp = 5;
 
 %Gcl_vel = (Gvel2*Kp_vel*CI_vel)/(1 + Gvel2*Kp_vel*CI_vel*CD_vel);
 
