be1163bb1b97db8aeea7567f2092fd4ad52f4acb
theodorgto <66820813+theodorgto@users.noreply.github.com>
2022-04-05 14:55:43 +0200

    seperate class PID

--- testscript.py
+++ testscript.py
@@ -3,6 +3,7 @@ print("Starting up...")
 import matplotlib.pyplot as plt
 import numpy as np
 import time, sys
+from classes.pid import PID
 from collections import deque
 
 # Import functionality of RTD measurement device
@@ -14,30 +15,6 @@ from lucidIo import IoReturn
 
 # Import functionality of power supply unit
 import ea_psu_controller as ea
-
-class PID(): ##added by theo
-    def __init__(self):
-        config = open('config.txt', 'r')
-        self.kp = float(config.readline())
-        self.taui = float(config.readline())
-        self.ki = self.kp/self.taui
-
-    integral_error = 0
-    error = 0
-    
-    def update_error(self,t,t_target):
-        self.error = t_target - t
-        if abs(self.error) < 10: # only activate integrator if error is less than 5 C
-            self.integral_error = self.integral_error + self.error
-        else:
-            self.integral_error = 0 # reset      #TODO check if this causes errors later?
-    
-    def integral(self):
-        return self.ki*self.integral_error
-
-    def proportional(self):
-        return self.kp*self.error
-
 
 # Initialize the LucidControl RTD measurement device. Can be /dev/ttyACM0 or /dev/ttyACM1:
 print("Connecting to /dev/ttyACM", end="")
