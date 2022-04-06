45f17ac1074721d1a2c7333798a4b71404e7ab75
leouhre <leouhrejakobsen@gmail.com>
2022-04-06 14:07:27 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,33 @@
 import socket
 import sys
+import time
  
 # Create a connection to the server application on port 81
 tcp_socket = socket.create_connection(('192.168.137.1', 4000))
+
+timer = time.time()
+count = 0
+msg1 = 'Warming up...'
+msg2 = '...'
+msg3 = 'ready'
+
+while True:
+    # True every 5 seconds
+    if time.time() - timer > 5:
+        tcp_socket.sendall(msg1.encode())
+#        if time.time() - timer > 5:
+#            tcp_socket.sendall(msg2.encode())
+        count += 1
+        timer = time.time()
+        if count == 5:
+            tcp_socket.sendall(msg3.encode())
+            break
+
  
-try:
-    data = 'Hi. I am a TCP client sending data to the server'
-    tcp_socket.sendall(data.encode())
- 
-finally:
-    print("Closing socket")
-    tcp_socket.close()
+#try:
+#    data = 'Hi. I am a TCP client sending data to the server'
+#    tcp_socket.sendall(data.encode())
+
+#finally:
+print("Closing socket")
+tcp_socket.close()
