8397439b75cd809c4d09e285b908ac0c099ceb9a
theodorgto <66820813+theodorgto@users.noreply.github.com>
2022-04-08 18:15:46 +0200

    as

--- pid_Test.py
+++ pid_Test.py
@@ -7,19 +7,7 @@ from classes.pid import PID
 PI = PID()
 
 
-d1 = deque()
-d1.append(1)
-d1.append(1)
-d1.append(1)
-
-d2 = deque()
-d2.append(2)
-d2.append(2)
-d2.append(2)
-values = (d1,d2)
-print(str(sum(d1)))
-print(str(sum(values.pop)))
     
 timer = time.time()
 
-print(str(np.random.random_sample()))
+print(np.random.random_sample())
