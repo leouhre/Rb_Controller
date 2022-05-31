d6e44892eeb655891068ff905c27529c3633aefb
leouhre <leouhrejakobsen@gmail.com>
2022-05-31 13:55:50 +0200

    matlab

--- matlab/tf_estimate.m
+++ matlab/tf_estimate.m
@@ -1,18 +1,17 @@
 close all
+clear all
 
 % Get step response from data file and save in variable 'temp'
-fileID = fopen('step_response/sensor0.txt', 'r');
+fileID = fopen('step_response/new_setup2_avg.txt', 'r');
 formatSpec = '%f';
 sizeA = [1,Inf];
 temp = fscanf(fileID,formatSpec,sizeA);
-% Use only every 10th sample since fs = 10Hz
-temp = temp(1 : 20 : end);
 fclose(fileID);
 
 % Create input/output vectors for the tfest() function
-% Remember to subtract room temperature, temp(2,1)
-u = transpose([zeros(1,length(temp)),15*ones(1,length(temp))]);
-y = transpose([zeros(1,length(temp)),temp - temp(1)]);
+% Remember to subtract room temperature (min(temp))
+u = transpose([zeros(1,length(temp)),16.5*ones(1,length(temp))]);
+y = transpose([zeros(1,length(temp)),temp - min(temp)]);
 
 % Uncomment following to see input/output data
 %figure(1)
@@ -23,24 +22,27 @@ y = transpose([zeros(1,length(temp)),temp - temp(1)]);
 %ylabel('temperature (degree C)')
 %hold off
 
-% Estimate the transfer function for the system (np = number of poles)
-data = iddata(y,u,1);
-np = 2;
+% Estimate the transfer function for the system
+% Set sample period Ts for iddata()
+data = iddata(y,u,0.5);
+np = 3;
 nz = 1;
 sys = tfest(data,np,nz);
-%sys2 = tfest(data,1,0);
-%sys3 = tfest(data,2,0);
+%sys2 = tfest(data,3,1);
+%sys3 = tfest(data,3,2);
 
 % To validate the system transfer function, try simulating a 15V response
-%figure(2)
-%opt = stepDataOptions('StepAmplitude',15);
-%step(sys,opt)
-%hold on
+figure(2)
+opt = stepDataOptions('StepAmplitude',16.5);
+step(sys,opt)
+hold on
 %step(sys2,opt)
 %step(sys3,opt)
-%plot(y(length(temp):length(y)))
-%legend('2 poles, 1 zero', '1 pole, 0 zeros', '2 poles, 0 zeros', 'Measured data')
-%hold off
+plot(downsample(y(length(temp):length(y)),2))
+xlim([0 length(temp)/2 + 100])
+legend('Estimated system', 'Measured data')
+%legend('2 poles, 1 zero', '3 poles, 1 zeros', '3 poles, 2 zeros', 'Measured data')
+hold off
 %%
 
 % Get numerator and denominator for Simulink model
@@ -50,18 +52,27 @@ G = tf(num,den);
 
 % These are the parameters to play with in order to obtain the optimal
 % controller
-Ni = 10; 
-pm = 50;
+Ni = 4.5; 
+pm = 45;
 alpha = 0.1;
-fs = 3;
+fs = 2;
 Ts = 1/fs;
+
+%D_active = 1; % PI-Lead controller
+D_active = 0; % PI controller
+I_active = 1; % PI controller
+%I_active = 0; % P controller
 
 % Calculate the negative phase contribution at the new crossover frequency 
 % due to the integrator as well as the positive contribution from the 
 % differentiator. Finally, calculate the phase shift at which the new 
 % crossover frequency is placed. 
 rho_i = rad2deg(-atan(1/Ni));
-rho_m = rad2deg(asin((1-alpha)/(1+alpha)));
+if D_active == 1
+    rho_m = rad2deg(asin((1-alpha)/(1+alpha)));
+else
+    rho_m = 0;
+end
 rho_G = pm - rho_i - rho_m - 180;
 
 % Find the new crossover frequency. Since the system contains 2 poles and 1
@@ -69,8 +80,11 @@ rho_G = pm - rho_i - rho_m - 180;
 % be chosen to be < 1 to obtain a larger phase shift contribution from 
 % the integrator, or a desired crossover frequency can be chosen, since
 % the system won't be unstable (in theory) for any frequencies. 
-wc = 3; % PI-Lead and PI controller
+%wc = 3; % PI-Lead and PI controller
 %wc = 9; % P controller
+[mag0,phase0,wout0] = bode(sys);
+k0 = find(phase0(:) > rho_G, 1, 'last');
+wc = wout0(k0);
 
 % Find the time constants of the controllers and define the transfer
 % functions of the controller parts
@@ -91,13 +105,9 @@ CI = tf([ti 1],[ti 0]);
 % that the gain found is actually at f = wc. Otherwise inspect "wout" to
 % see if the margin of the find() command should be smaller/larger.
 %[mag,phase,wout] = bode(G*CD*CI,{0.01,30}); % PI-Lead controller
-[mag,phase,wout] = bode(G*CI,{0.01,30}); % PI controller
-k = find(abs(wout(:) - wc) < 0.2, 1, 'first');
-kp = 1/mag(1,1,k);
-%D_active = 1; % PI-Lead controller
-D_active = 0; % PI controller
-I_active = 1; % PI controller
-%I_active = 0; % P controller
+[mag,phase,wout] = bode(G*CI,{wc/10,10*wc}); % PI controller
+k = find(wout < wc, 1, 'last');
+kp = 1/mag(k);
 if D_active == 1
     G_ol = kp*G*CI*CD; % PI-Lead controller
     G_cl = kp*G*CD*CI/(1+kp*G*CD*CI); % PI-Lead controller
@@ -123,7 +133,7 @@ title("Bode plot of PI controller");
 grid on
 hold off
 
-h = sim(model,1000);
+h = sim(model,3000);
 plot(h)
 
 %load_system(model);
