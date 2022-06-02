b4955c77984e044f4318fd7e2dc5f9db39fb936e
theodorgto <66820813+theodorgto@users.noreply.github.com>
2022-06-02 11:27:08 +0200

    shelve

--- classes/pid2.py
+++ classes/pid2.py
@@ -1,28 +1,19 @@
 from pathlib import Path
+import shelve
 
 class PID2():
     P = Path('C:\\','Users','leouh', 'Documents', 'Rb_Controller')
     def __init__(self):
-        with open('config.txt', 'r') as config:
-            self.kp = float(config.readline())
-            self.ki = float(config.readline())
-            self.kd = float(config.readline())
-            self.temperature_limit = float(config.readline())
-            self.temperature_offset = float(config.readline())
-            self.max_fluctuations = float(config.readline())
-            self.settle_slope = float(config.readline())
-            self.slope_length = float(config.readline())
-            self.settle_wait_time = float(config.readline())
-            self.alpha = float(config.readline())
-            self.freq = float(config.readline())
-        self.Ts = 1/self.freq
+        with shelve.open('config') as config:
+            self.cfg = dict(config)
+
+        self.Ts = 1/self.cfg['freq']
         # Avoid division with zero if integral gain is 0 (disabled)
-        if self.ki:
-            self.taui = self.kp/self.ki
-        if self.kd:
-            self.taud = self.kd/self.kp
+        if self.cfg['ki']:
+            self.taui = self.cfg['kp']/self.cfg['ki']
+        if self.cfg['kd']:
+            self.taud = self.cfg['kd']/self.cfg['kp']
         
-    
     integral_error = 0
     prev_t = 0
 
@@ -36,36 +27,36 @@ class PID2():
     settlecount = 0
 
     def settle_update(self, t, t_target):
-        if abs(t_target - t) < self.max_fluctuations:
+        if abs(t_target - t) < self.cfg['max_fluctuations']:
             self.settlecount += 1
         else:
             self.settlecount = 0
 
     def settle_check(self):
-        return self.settlecount > self.settle_wait_time/self.freq
+        return self.settlecount > self.cfg['settle_wait_time']/self.cfg['freq']
 
     # Simple PID controller. Explicitly dealing with wind-up
     def update(self, t, t_target):
         error = t_target - t
-        p = self.kp * error
-        i = self.ki * self.integral_error * self.Ts
+        p = self.cfg['kp'] * error
+        i = self.cfg['ki'] * self.integral_error * self.Ts
         derivative = t - self.prev_t
-        d = self.kd * derivative / self.Ts
+        d = self.cfg['kd'] * derivative / self.Ts
         self.prev_t = t
         pidout = p + i + d
-        #pidout *= self.kp
+        #pidout *= self.cfg['kp']
         if self.lower_lim < pidout < self.upper_lim:
             self.integral_error += error
         return max(min(pidout,self.upper_lim),self.lower_lim)
 
     # PI-Lead controller implemented using [Wang 4.4.2]. Implicitly dealing with wind-up
     def update3(self, y_current, r_current):
-        if not self.ki:
+        if not self.cfg['ki']:
             print("update3() requires an PI or PI-Lead controller")
             return 0
-        uD_current = ((self.alpha*self.taud) / (self.alpha*self.taud + self.Ts)) * self.uD_past  + \
-            ((self.kp*self.taud) / (self.alpha*self.taud + self.Ts)) * (y_current - self.y_past)
-        u_current = self.u_past + self.kp*(self.y_past - y_current) + ((self.kp*self.Ts) / self.taui) * \
+        uD_current = ((self.cfg['alpha']*self.taud) / (self.cfg['alpha']*self.taud + self.Ts)) * self.uD_past  + \
+            ((self.cfg['kp']*self.taud) / (self.cfg['alpha']*self.taud + self.Ts)) * (y_current - self.y_past)
+        u_current = self.u_past + self.cfg['kp']*(self.y_past - y_current) + ((self.cfg['kp']*self.Ts) / self.taui) * \
             (r_current - y_current) - uD_current + self.uD_past
         u_current = max(min(u_current,self.upper_lim),self.lower_lim)
         self.uD_past = uD_current
