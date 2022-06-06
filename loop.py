0c276f8f625732ec7e81fee4f3ed8f6d0704329f
theodorgto <66820813+theodorgto@users.noreply.github.com>
2022-06-06 15:07:45 +0200

    Update loop.py

--- loop.py
+++ loop.py
@@ -209,11 +209,6 @@ class loop(threading.Thread):
             globals.error_msg = "PSU lost. Restart required"
             self.safemsg_matlab("PSU lost. Restart required")
             self.safeexit()
-        except Exception as ex:
-            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-            message = template.format(type(ex).__name__, ex.args)
-            print (message)
-            self.safeexit()
 
         if globals.BYPASS_MODE:
             self.psu.output_off()
