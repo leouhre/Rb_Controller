be097732aa62e82b9f5d04eac6b42a2c3d0659ea
theodorgto <66820813+theodorgto@users.noreply.github.com>
2022-06-07 18:46:01 +0200

    Update pid.py

--- classes/pid.py
+++ classes/pid.py
@@ -49,9 +49,11 @@ class PID():
         if to == 1:
             self.integral_error = self.integral_error2*self.cfg['ki2']/self.cfg['ki']
             print(f"PID{to}")
+            print(self.integral_error)
         if to == 2:
             self.integral_error2 = self.integral_error*self.cfg['ki']/self.cfg['ki2']
             print(f"PID{to}")
+            print(self.integral_error2)
 
 
     # Simple PID controller. Explicitly dealing with wind-up
